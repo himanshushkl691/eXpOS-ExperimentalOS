d6d571c1bf472fba5a43816d60424cf9bab640b9
Himanshu Shukla <himanshushkl691@gmail.com>
2019-08-23 15:58:58 +0530

    Stage 14 completed

--- batch.bat
+++ batch.bat
@@ -1,8 +1,11 @@
 load --os $HOME/Desktop/myexpos/spl/spl_progs/final_os_startup.xsm
-load --init $HOME/Desktop/myexpos/expl/sample/number.xsm
+load --init $HOME/Desktop/myexpos/expl/sample/odd.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/even.xsm
 load --idle $HOME/Desktop/myexpos/expl/sample/idle.xsm
 load --exhandler $HOME/Desktop/myexpos/spl/spl_progs/haltprog.xsm
 load --library $HOME/Desktop/myexpos/expl/library.lib
-load --int=timer $HOME/Desktop/myexpos/spl/spl_progs/sample_timer.xsm
+load --int=timer $HOME/Desktop/myexpos/spl/spl_progs/timer_handler.xsm
 load --int=7 $HOME/Desktop/myexpos/spl/spl_progs/sample_int_7.xsm
-load --int=10 $HOME/Desktop/myexpos/spl/spl_progs/haltprog.xsm
+load --int=10 $HOME/Desktop/myexpos/spl/spl_progs/int_10.xsm
+load --module 5 $HOME/Desktop/myexpos/spl/spl_progs/module_5.xsm
+load --module 7 $HOME/Desktop/myexpos/spl/spl_progs/module_7.xsm
