1fff9a44d63f5160e75df9b1add239087537f0ca
Himanshu Shukla <himanshushkl691@gmail.com>
2019-08-29 20:29:20 +0530

    Stage 17 complete

--- batch.bat
+++ batch.bat
@@ -1,5 +1,5 @@
 load --os $HOME/Desktop/myexpos/spl/spl_progs/final_os_startup.xsm
-load --init $HOME/Desktop/myexpos/expl/sample/gcd.xsm
+load --init $HOME/Desktop/myexpos/expl/sample/even.xsm
 load --idle $HOME/Desktop/myexpos/expl/sample/idle.xsm
 load --exhandler $HOME/Desktop/myexpos/spl/spl_progs/haltprog.xsm
 load --library $HOME/Desktop/myexpos/expl/library.lib
@@ -7,8 +7,11 @@ load --int=timer $HOME/Desktop/myexpos/spl/spl_progs/timer_handler.xsm
 load --int=console $HOME/Desktop/myexpos/spl/spl_progs/console_interrupt.xsm
 load --int=6 $HOME/Desktop/myexpos/spl/spl_progs/int_6.xsm
 load --int=7 $HOME/Desktop/myexpos/spl/spl_progs/sample_int_7.xsm
+load --int=9 $HOME/Desktop/myexpos/spl/spl_progs/int_9.xsm
 load --int=10 $HOME/Desktop/myexpos/spl/spl_progs/int_10.xsm
 load --module 0 $HOME/Desktop/myexpos/spl/spl_progs/module_0_stage15.xsm
+load --module 1 $HOME/Desktop/myexpos/spl/spl_progs/module_1.xsm
+load --module 2 $HOME/Desktop/myexpos/spl/spl_progs/module_2.xsm
 load --module 4 $HOME/Desktop/myexpos/spl/spl_progs/module_4_stage15.xsm
 load --module 5 $HOME/Desktop/myexpos/spl/spl_progs/module_5.xsm
 load --module 7 $HOME/Desktop/myexpos/spl/spl_progs/module_7.xsm
