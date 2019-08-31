7e01330c5ee1c15da00650335f80645d31e8d704
Himanshu Shukla <himanshushkl691@gmail.com>
2019-08-31 20:42:54 +0530

    Stage 18 completed

--- batch.bat
+++ batch.bat
@@ -1,11 +1,13 @@
 load --os $HOME/Desktop/myexpos/spl/spl_progs/final_os_startup.xsm
 load --init $HOME/Desktop/myexpos/expl/sample/shell_1.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/even.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/gcd.xsm
 load --idle $HOME/Desktop/myexpos/expl/sample/idle.xsm
 load --exhandler $HOME/Desktop/myexpos/spl/spl_progs/haltprog.xsm
 load --library $HOME/Desktop/myexpos/expl/library.lib
 load --int=timer $HOME/Desktop/myexpos/spl/spl_progs/timer_handler.xsm
 load --int=console $HOME/Desktop/myexpos/spl/spl_progs/console_interrupt.xsm
+load --int=disk $HOME/Desktop/myexpos/spl/spl_progs/disk_interrupt.xsm
 load --int=6 $HOME/Desktop/myexpos/spl/spl_progs/int_6.xsm
 load --int=7 $HOME/Desktop/myexpos/spl/spl_progs/sample_int_7.xsm
 load --int=9 $HOME/Desktop/myexpos/spl/spl_progs/int_9.xsm
