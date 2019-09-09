ad4710ffca333c2e34dcf19f90915608cbe5cf65
Himanshu Shukla <himanshushkl691@gmail.com>
2019-09-10 02:53:21 +0530

    Stage 21 completed

--- batch.bat
+++ batch.bat
@@ -1,10 +1,10 @@
 load --os $HOME/Desktop/myexpos/spl/spl_progs/final_os_startup.xsm
-load --init $HOME/Desktop/myexpos/expl/sample/shell_1.xsm
+load --init $HOME/Desktop/myexpos/expl/sample/shell_2.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/gcd.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/pid.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/llist.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/even.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/bubble.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/odd.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/fork21.xsm
 load --idle $HOME/Desktop/myexpos/expl/sample/idle.xsm
 load --exhandler $HOME/Desktop/myexpos/spl/spl_progs/exception_handler.xsm
 load --library $HOME/Desktop/myexpos/expl/library.lib
@@ -16,6 +16,8 @@ load --int=7 $HOME/Desktop/myexpos/spl/spl_progs/sample_int_7.xsm
 load --int=8 $HOME/Desktop/myexpos/spl/spl_progs/int_8.xsm
 load --int=9 $HOME/Desktop/myexpos/spl/spl_progs/int_9.xsm
 load --int=10 $HOME/Desktop/myexpos/spl/spl_progs/int_10.xsm
+load --int=11 $HOME/Desktop/myexpos/spl/spl_progs/int_11.xsm
+load --int=15 $HOME/Desktop/myexpos/spl/spl_progs/haltprog.xsm
 load --module 0 $HOME/Desktop/myexpos/spl/spl_progs/module_0_stage15.xsm
 load --module 1 $HOME/Desktop/myexpos/spl/spl_progs/module_1.xsm
 load --module 2 $HOME/Desktop/myexpos/spl/spl_progs/module_2.xsm
