7088dd062eb64757d22836005e527366e3378de8
Himanshu Shukla <himanshushkl691@gmail.com>
2019-09-11 05:14:55 +0530

    Stage 22 completed

--- batch.bat
+++ batch.bat
@@ -1,10 +1,14 @@
 load --os $HOME/Desktop/myexpos/spl/spl_progs/final_os_startup.xsm
 load --init $HOME/Desktop/myexpos/expl/sample/shell_2.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/gcd.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/parent.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/sem22.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/pid.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/llist.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/bubble.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/child.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/fork21.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/test.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/conMS.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/seqMS.xsm
 load --idle $HOME/Desktop/myexpos/expl/sample/idle.xsm
 load --exhandler $HOME/Desktop/myexpos/spl/spl_progs/exception_handler.xsm
 load --library $HOME/Desktop/myexpos/expl/library.lib
@@ -17,6 +21,8 @@ load --int=8 $HOME/Desktop/myexpos/spl/spl_progs/int_8.xsm
 load --int=9 $HOME/Desktop/myexpos/spl/spl_progs/int_9.xsm
 load --int=10 $HOME/Desktop/myexpos/spl/spl_progs/int_10.xsm
 load --int=11 $HOME/Desktop/myexpos/spl/spl_progs/int_11.xsm
+load --int=13 $HOME/Desktop/myexpos/spl/spl_progs/int_13.xsm
+load --int=14 $HOME/Desktop/myexpos/spl/spl_progs/int_14.xsm
 load --int=15 $HOME/Desktop/myexpos/spl/spl_progs/haltprog.xsm
 load --module 0 $HOME/Desktop/myexpos/spl/spl_progs/module_0_stage15.xsm
 load --module 1 $HOME/Desktop/myexpos/spl/spl_progs/module_1.xsm
