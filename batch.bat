66164c15cb9c81bed28fa4ba50066a3687195eab
Himanshu Shukla <himanshushkl691@gmail.com>
2019-10-07 16:55:06 +0530

    Stage 23 completed

--- batch.bat
+++ batch.bat
@@ -5,8 +5,8 @@ load --exec $HOME/Desktop/myexpos/expl/sample/sem22.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/pid.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/llist.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/child.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/fork21.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/test.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/delete.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/create.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/conMS.xsm
 load --exec $HOME/Desktop/myexpos/expl/sample/seqMS.xsm
 load --idle $HOME/Desktop/myexpos/expl/sample/idle.xsm
@@ -15,6 +15,7 @@ load --library $HOME/Desktop/myexpos/expl/library.lib
 load --int=timer $HOME/Desktop/myexpos/spl/spl_progs/timer_handler.xsm
 load --int=console $HOME/Desktop/myexpos/spl/spl_progs/console_interrupt.xsm
 load --int=disk $HOME/Desktop/myexpos/spl/spl_progs/disk_interrupt.xsm
+load --int=4 $HOME/Desktop/myexpos/spl/spl_progs/int_4.xsm
 load --int=6 $HOME/Desktop/myexpos/spl/spl_progs/int_6.xsm
 load --int=7 $HOME/Desktop/myexpos/spl/spl_progs/sample_int_7.xsm
 load --int=8 $HOME/Desktop/myexpos/spl/spl_progs/int_8.xsm
@@ -23,7 +24,7 @@ load --int=10 $HOME/Desktop/myexpos/spl/spl_progs/int_10.xsm
 load --int=11 $HOME/Desktop/myexpos/spl/spl_progs/int_11.xsm
 load --int=13 $HOME/Desktop/myexpos/spl/spl_progs/int_13.xsm
 load --int=14 $HOME/Desktop/myexpos/spl/spl_progs/int_14.xsm
-load --int=15 $HOME/Desktop/myexpos/spl/spl_progs/haltprog.xsm
+load --int=15 $HOME/Desktop/myexpos/spl/spl_progs/int_15.xsm
 load --module 0 $HOME/Desktop/myexpos/spl/spl_progs/module_0_stage15.xsm
 load --module 1 $HOME/Desktop/myexpos/spl/spl_progs/module_1.xsm
 load --module 2 $HOME/Desktop/myexpos/spl/spl_progs/module_2.xsm
