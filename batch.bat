94da334a10a501b153c70524077acf4f8a9a76a7
Himanshu Shukla <himanshushkl691@gmail.com>
2019-10-08 04:03:17 +0530

    Stage 24 completed

--- batch.bat
+++ batch.bat
@@ -1,14 +1,6 @@
 load --os $HOME/Desktop/myexpos/spl/spl_progs/final_os_startup.xsm
 load --init $HOME/Desktop/myexpos/expl/sample/shell_2.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/parent.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/sem22.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/pid.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/llist.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/child.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/delete.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/create.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/conMS.xsm
-load --exec $HOME/Desktop/myexpos/expl/sample/seqMS.xsm
+load --exec $HOME/Desktop/myexpos/expl/sample/readf.xsm
 load --idle $HOME/Desktop/myexpos/expl/sample/idle.xsm
 load --exhandler $HOME/Desktop/myexpos/spl/spl_progs/exception_handler.xsm
 load --library $HOME/Desktop/myexpos/expl/library.lib
@@ -16,6 +8,7 @@ load --int=timer $HOME/Desktop/myexpos/spl/spl_progs/timer_handler.xsm
 load --int=console $HOME/Desktop/myexpos/spl/spl_progs/console_interrupt.xsm
 load --int=disk $HOME/Desktop/myexpos/spl/spl_progs/disk_interrupt.xsm
 load --int=4 $HOME/Desktop/myexpos/spl/spl_progs/int_4.xsm
+load --int=5 $HOME/Desktop/myexpos/spl/spl_progs/int_5.xsm
 load --int=6 $HOME/Desktop/myexpos/spl/spl_progs/int_6.xsm
 load --int=7 $HOME/Desktop/myexpos/spl/spl_progs/sample_int_7.xsm
 load --int=8 $HOME/Desktop/myexpos/spl/spl_progs/int_8.xsm
@@ -28,6 +21,7 @@ load --int=15 $HOME/Desktop/myexpos/spl/spl_progs/int_15.xsm
 load --module 0 $HOME/Desktop/myexpos/spl/spl_progs/module_0_stage15.xsm
 load --module 1 $HOME/Desktop/myexpos/spl/spl_progs/module_1.xsm
 load --module 2 $HOME/Desktop/myexpos/spl/spl_progs/module_2.xsm
+load --module 3 $HOME/Desktop/myexpos/spl/spl_progs/module_3.xsm
 load --module 4 $HOME/Desktop/myexpos/spl/spl_progs/module_4_stage15.xsm
 load --module 5 $HOME/Desktop/myexpos/spl/spl_progs/module_5.xsm
 load --module 7 $HOME/Desktop/myexpos/spl/spl_progs/module_7.xsm
